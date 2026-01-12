ed631d85501ee56fac5f5540c984acd053cb72b4
Gabriel Baptista <software.architecture@outlook.com>
2026-01-12 20:43:14 -0300

    Fix e-mails

---
+++ ch08/wwtravelclub/Pages/Emails.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ch08.Pages
+{
+    public class EmailsModel : PageModel
+    {
+        private readonly ILogger<EmailsModel> _logger;
+
+        public EmailsModel(ILogger<EmailsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
